c93665d4b4c16ad22d4877b06e6165d9f879a446
Aniket Lokhande <Aniket.Lokhande@extentia.com>
2021-08-06 18:59:52 +0530

    added scrollmanger

---
+++ Source file/ScrollManger.swift
@@ -0,0 +1,19 @@
+//
+//  ScrollManger.swift
+//  AniScrollviewDemo
+//
+//  Created by Aniket Lokhande on 06/08/21.
+//
+
+import Foundation
+public struct ScrollManger {
+    
+    
+    public init(){
+        
+    }
+    public func helloToYou() -> String{
+        return "Hello To You"
+    }
+    
+}
